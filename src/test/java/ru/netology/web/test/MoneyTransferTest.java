b1dd8763107b2e7eb18edf1552823b9a79cf5626
Anna Belousova <annabelousovva@gmail.com>
2020-11-13 22:10:15 +0300

    Add Tests

---
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -0,0 +1,23 @@
+package ru.netology.web.test;
+
+import lombok.Data;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.LoginPageV1;
+
+import static com.codeborne.selenide.Selenide.open;
+
+
+class MoneyTransferTest {
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV1(){
+        open("http://localhost:9999");
+        val loginPage = new LoginPageV1();
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+}
